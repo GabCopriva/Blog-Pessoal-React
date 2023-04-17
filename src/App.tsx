7108c6eb69ddae00f5029e6dee4d2354246fa758
GabCopriva <contatogabrielcopriva@gmail.com>
2023-04-17 20:04:37 -0300

    Atualização

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/estaticos/Navbar/Navbar';
 import Footer from './components/estaticos/Footer/Footer';
+import CadastroUsuario from './Paginas/cadastroUsuario/CadastroUsuario';
 import Home from './Paginas/Home/Home'
 import Login from './Paginas/Login/Login'
 import './App.css'
@@ -16,7 +17,12 @@ function App() {
 
         <Routes> 
           <Route path="/" element={<Login />} />
+
           <Route path="/home" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+          
+          <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
           </Routes>
           
       <Footer />
